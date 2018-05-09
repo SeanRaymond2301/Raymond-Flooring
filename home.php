dc4fdbc3eb71227bf95d9e56ba0ea9b80c78d0e2
Paulson Brandon <150087553@cart.ads>
2018-05-09 08:21:37 -0700

    fixes

--- home.php
+++ home.php
@@ -4,9 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Home</title>
+  
 </head>
 <body>
+  
+  <h1>Flooring Made Simple</h1>
+  
+   <h2>Matthew Raymond</h2>
+   <p>License #768883</p>
+   
+   
     <img src="http://via.placeholder.com/100x100" alt="">
     <nav>
         <div>
