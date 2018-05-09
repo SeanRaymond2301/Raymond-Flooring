95e70cfe94da7725b6259b535d6e31b0e9017fa6
Mukai Christian <310047593@cart.ads>
2018-05-09 13:19:13 -0700

    moved footer and header out of pages folder

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<!--there will be php title here -->
+<?php
+	$title = //will figure out how to do this later.
+?>
 
 <header>
     <nav>
