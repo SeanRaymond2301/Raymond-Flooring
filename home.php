833b80a353c30118be0e86ce49d858b5f3339779
Paulson Brandon <150087553@cart.ads>
2018-05-09 08:40:52 -0700

    startups

--- home.php
+++ home.php
@@ -17,23 +17,25 @@
    
     <img src="http://via.placeholder.com/100x100" alt="">
     
-    <h3>Vinyl Plank</h3>
+    <h3>Free-Floating Vinyl Plank</h3>
     <h4>Engineered Hardwood</h4>
     <h5>Laminate</h5>
+    <h6>Carpet</h6>
     
     <nav>
         <div>
             <li>Read More</li>
             <li>Read More</li>
-            <li>Read More</li>        
+            <li>Read More</li>  
+            <li>Read More</li>      
         </div>
     </nav>
     
-    <h6>About Me</h6>
-    <p></p>
+    <h7>About Me</h7>
+    <p>I started in the flooring busniness with my dad installing carpet in 1985. I installed carpet with him until we opened a very small store. There I learned the supply and sales aspect of the flooring industry... </p>
     
-    <h7>Questionaire</h7>
-    <p></p>
+    <h8>Questionaire</h8>
+    <p>This is a questionaire to help determine what type of flooring is best for your situation and to match your best interest.</p>
     
     </body>
 </html>
