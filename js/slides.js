2a5390664987a802ca4bfc8edeb612d9f633cc78
Raymond Sean <310047892@cart.ads>
2018-05-10 14:16:42 -0700

    Added the rest of the pages we will need. Also fixed the compiler.

--- js/slides.js
+++ js/slides.js
@@ -1,32 +1,31 @@
 	//I will leave this here, I am going to write one that automatically goes, and loops through as well. I will show my dad both and figure out which one he likes.
 var slideIndex = 1;
-showSlides(slideIndex);
+	showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-	console.log("didathing");
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].classList.remove("active");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].classList.add("active");
+var i,
+    slides = document.getElementsByClassName("mySlides"),
+	dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].classList.remove("active");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].classList.add("active");
 }
 
 // function showSlide(n) {
--- pages/DNE.php
+++ pages/DNE.php
@@ -1,2 +1,2 @@
 <h2>Sorry!</h2>
-	<p>The page you were looking for could not be found. Please return to the <a href="?page=Home">home page.</a></p>
+	<p>The page you were looking for could not be found. Please return to the <a href="?page=Home">Home.</a></p>
