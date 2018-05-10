c4363e5b4317776a84f1b708ae2fc91c575d9038
Paulson Brandon <150087553@cart.ads>
2018-05-10 09:58:44 -0700

    slideshow

--- compilescss.js
+++ compilescss.js
@@ -79,12 +79,31 @@ fs.readdir(sassFolder + "/", function (err, items) {
     }
 });
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
-//Watch the directory
-fs.watch(sassFolder + "/", {
-    recursive: true,
-    encoding: "utf8"
-}, fileChanged);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-//Tell the user that it has started watching
-console.log("Watching all files in " + sassFolder);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
