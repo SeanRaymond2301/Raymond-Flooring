8fe1ff87b04be2b8f9d59828ee3d70b2d905ef41
Mukai Christian <310047593@cart.ads>
2018-05-10 15:11:41 -0700

    Forgot to close xampp and atom

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
-
+<link rel="stylesheet" href="css/main.css">
     <footer>
         <p>&copy; 2018 Matthew Raymond</p>
-         <img src="http://via.placeholder.com/50x50" alt="">
+         <img src="http://via.placeholder.com/50x50" alt="" class="footer-logo">
         <div class="footer-nav">
             <ul>
-                <a href="?page=Home">
+                <a href="home.php">
                     <li>Home</li>
                 </a>
-                <a href="?page=About">
-                    <li>About Me</li>
+                <a href="aboutme.php">
+                    <li>About </li>
                 </a>
                 <a href="?page=Hardwood">
                     <li>Hardwood</li>
