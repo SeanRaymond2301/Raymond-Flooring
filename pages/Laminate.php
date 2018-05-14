04d90727a7c81ad68acff2d1a3db6a5ff278cdbc
Raymond Sean <310047892@cart.ads>
2018-05-14 15:21:04 -0700

    Begun styling one of the content pages so I can use it as a template to use the database to build the content of the page instead

--- pages/Laminate.php
+++ pages/Laminate.php
@@ -1,17 +1,19 @@
-<h2>Laminate</h2>
+<main class="content">
+	<h2>Laminate</h2>
 
-<p>Pro's</p>
-<ul>
-    <li>Hard surface, hard to gouge (an indention or groove made by a cut or blunt object).</li>
-    <li>Mimics the appearance to real wood.</li>
-</ul>
+	<p>Pro's</p>
+	<ul>
+	    <li>Hard surface, hard to gouge (an indention or groove made by a cut or blunt object).</li>
+	    <li>Mimics the appearance to real wood.</li>
+	</ul>
 
-<br>
+	<br>
 
-<p>Con's</p>
-<ul>
-    <li>Is the loudest of the three.</li>
-    <li>Susceptible to water damage.</li>
-    <li>Requires more molding than others.</li>
-    <li>Expands and contracts the most in different temperatures and humidities.</li>
-</ul>
+	<p>Con's</p>
+	<ul>
+	    <li>Is the loudest of the three.</li>
+	    <li>Susceptible to water damage.</li>
+	    <li>Requires more molding than others.</li>
+	    <li>Expands and contracts the most in different temperatures and humidities.</li>
+	</ul>
+</main>
