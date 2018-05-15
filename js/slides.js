a7a4ad5accbdde540c4f2b42c5039c92ef74b218
Raymond Sean <310047892@cart.ads>
2018-05-15 14:35:25 -0700

    Fixed an issue that broke the slideshow, as well as made the slideshow animation less harsh because it hurt.

--- js/slides.js
+++ js/slides.js
@@ -16,7 +16,9 @@ function showSlides(n) {
 var i,
     slides = document.getElementsByClassName("mySlides"),
 	dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {slideIndex = 1}
+	if (n > slides.length) {
+		slideIndex = 1
+	}
 	if (n < 1) {slideIndex = slides.length}
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
--- pages/home.php
+++ pages/home.php
@@ -2,36 +2,37 @@
 <div class="slideshow-container">
   <!-- Full-width images with number and caption text -->
 
-  <div class="mySlides fade">
+<div class="mySlides fade">
     <img src="images/hardwoodhome.jpg" style="width:100%">
     <div class="text">Engineered Hardwood
-    <p>This is a realistic looking type of flooring to real wood, and is fairly quiet.</p>
-    <ul>
-        <li class="black"><a href="?page=EngineeredHardwood" style="black">Read More</a></li>
-    </ul>
+	    <p>This is a realistic looking type of flooring to real wood, and is fairly quiet.</p>
+	    <ul>
+	        <li class="black"><a href="?page=EngineeredHardwood" class="black">Read More</a></li>
+	    </ul>
+	</div>
+</div>
     <!-- Full-width images with number and caption text -->
-
-    <div class="mySlides fade">
-        <img src="http://via.placeholder.com/1920x570" style="width:100%">
-        <div class="text">Engineered Hardwood
-            <p>This is a realistic looking type of flooring to real wood, and is fairly quiet.</p>
-            <ul>
-                <li class="black"><a href="?page=EngineeredHardwood" style="black">Read More</a></li>
-            </ul>
-        </div>
-    </div>
 
     <div class="mySlides fade">
         <img src="images/laminate-6.jpg" style="width:100%">
         <div class="text">Laminate
             <p>A hard surface, that mimics the look of real wood, but is the loudest of the three</p>
             <ul>
-                <li class="black"><a href="?page=Laminate">Read More</a></li>
+                <li class="black"><a href="?page=Laminate" class="black">Read More</a></li>
             </ul>
         </div>
     </div>
-  </div>
 
+  <div class="mySlides fade">
+	  <img src="http://via.placeholder.com/1920x570" style="width:100%">
+	  <div class="text">Vinyl Plank
+		  <p>Vinyl plank is the easiest to maintain, and the quietest of the three.</p>
+		  <ul>
+			  <li class="black"><a href="?page=EngineeredHardwood" class="black">Read More</a></li>
+		  </ul>
+	  </div>
+  </div>
+</div>
     <!-- Next and previous buttons -->
 
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
