0b79c026d8db827844fbd6c43a80b43164056a98
Raymond Sean <310047892@cart.ads>
2018-05-16 15:13:44 -0700

    Adding general content to the content pages, as well as changed font sizes to possible final sizes, as the current one was way too small.

--- pages/EngineeredHardwood.php
+++ pages/EngineeredHardwood.php
@@ -1,33 +1,38 @@
 <main class="content">
 	<div>
-		<img src="images/hardwood3.jpg" alt="" style="width:100%;">
-		<h2 style="text-align:center;" class="headerText">Engineered Hardwood</h2>
+		<img src="images/hardwood4.png" alt="" style="width:100%;">
+		<h1 style="text-align:center;" class="headerText">Engineered Hardwood</h1>
 	</div>
 	<div style="width:40%; margin: 0 auto;">
 		<div class="wrap">
 			<div class="comp headerText2">
 				<div>
-					<h3>Pro's</h3>
+					<h2>Pros</h2>
 					<ul>
-					    <li>Silent</li>
+					    <li>Quiet</li>
 					    <li>Real Wood</li>
-					    <li>Soft Surface</li>
+						<li>Best Looking of the 3</li>
+						<li>Most Valuable of the 3</li>
 					</ul>
 				</div>
 			</div>
 			<div class="comp headerText3">
 				<div>
-					<h3>Con's</h3>
+					<h2>Cons</h2>
 					<ul>
 					    <li>Susceptible to Water Damage</li>
-					    <li>Difficult to Repair</li>
 					    <li>Most Expensive</li>
-						<li>Hard to Install</li>
+						<li>Hard to Install/Repair</li>
 					    <li>Moldings</li>
 					    <li>Expands and Contracts</li>
+						<li>Easy to Damage</li>
 					</ul>
 				</div>
 			</div>
+		</div>
 	</div>
+	<div class="w50ma">
+		<p>Engineered hardwood is a very popular choice for those who enjoy a much more natural looking floor. This type of floor is real wood. It can be glued down or layed down on underlayement. Although the latter is less common, it is not any lesser of an option. The only difference functionally is underlayement will not be as quiet as glued down hardwood. Glued down is generally more expensive, and harder to install/repair as well.</p>
+		<p>However engineered hardwood is generally used to raise the value of a house. It is one of the more expensive aspects of retailing, as it can be very expensive to install large areas. It is also commonly chosen in retailing as it is the most appealing of the three looks wise. </p>
 	</div>
 </main>
