6e0c3503f8addbd0700a1cea534d9987f60ae584
Paulson Brandon <150087553@cart.ads>
2018-05-16 08:30:01 -0700

    laminate-vinyl plank

--- pages/Laminate.php
+++ pages/Laminate.php
@@ -3,20 +3,25 @@
 		<img src="images/hardwood3.jpg" alt="" style="width:100%;">
 		<h2 style="text-align:center;" class="headerText">Laminate</h2>
 	</div>
-
-	<p>Pro's</p>
+	<div style="width:40%; margin: 0 auto;">
+	<div class="wrap">
+<div class="comp">
+	<h3>Pro's</h3>
 	<ul>
 	    <li>Hard surface, hard to gouge (an indention or groove made by a cut or blunt object).</li>
 	    <li>Mimics the appearance to real wood.</li>
 	</ul>
-
-	<br>
-
-	<p>Con's</p>
+    </div>
+	
+<div class="comp">
+	<h3>Con's</h3>
 	<ul>
 	    <li>Is the loudest of the three.</li>
 	    <li>Susceptible to water damage.</li>
 	    <li>Requires more molding than others.</li>
 	    <li>Expands and contracts the most in different temperatures and humidities.</li>
 	</ul>
+        </div>
+    </div>
+    </div>
 </main>
--- pages/VinylPlank.php
+++ pages/VinylPlank.php
@@ -3,19 +3,24 @@
         <img src="images/vinyl-plank.jpg" alt="" style="width:100%;">
         <h2 style="text-align:center;" class="headerText">Vinyl Plank</h2>
     </div>
-
-    <p>Pro's</p>
+<div style="width:40%; margin: 0 auto;">
+   <div class="wrap">
+   <div class="comp">
+    <h3>Pro's</h3>
     <ul>
         <li>Waterproof.</li>
         <li>Very quiet.</li>
         <li>Easiest to maintain.</li>
         <li>Really soft.</li>
     </ul>
-
-    <br>
-
-    <p>Con's</p>
+       </div>
+    
+<div class="comp">
+    <h3>Con's</h3>
     <ul>
         <li>Easy to scratch.</li>
     </ul>
+       </div>
+    </div>
+    </div>
 </main>
