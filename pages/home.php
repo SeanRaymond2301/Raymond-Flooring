58fecdd7202a3c65849b9ed4aaaf833a3d735a87
Creason Breanna <562734@cart.ads>
2018-05-22 13:11:33 -0700

    updating on my side

--- pages/home.php
+++ pages/home.php
@@ -54,7 +54,7 @@
 <div class="bottomsection">
     <div class="wrapper">
         <h2>About Me</h2>
-        <p>I started in the flooring busniness with my dad installing carpet in 1985. I installed carpet with him until we opened a very small store. There I learned the supply and sales aspect of the flooring industry. In 1995 I left his business and started working for various companies. In 1995 I left his business and started... </p>
+        <p>I started in the flooring business with my dad installing carpet in 1985. I installed carpet with him until we opened a very small store. There I learned the supply and sales aspect of the flooring industry. In 1995 I left his business and started working for various companies. In 1995 I left his business and started... </p>
         <ul>
             <li class="black"><a href="?page=About" class="black">Read More</a></li>
         </ul>
