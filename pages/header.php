c12f0501d642189beeecca5a0c31f4775962bd2e
Zepeda Evelyn <150061083@cart.ads>
2018-05-25 10:28:12 -0700

    navbar dropdown

--- pages/header.php
+++ pages/header.php
@@ -18,38 +18,46 @@
 
 
 <header>
-        <nav>
-            <div class="logo imglogo"></div>
-            <div class="nav">
-                <ul>
-                    <a href="?page=Home">
-                        <li>Home</li>
-                    </a>
-                    <a href="?page=About">
-                        <li>About</li>
-                    </a>
-                    <a class="hide" href="?page=EngineeredHardwood">
-                        <li class="hide">Engineered Hardwood</li>
-                    </a>
+    <nav>
+        <div class="logo imglogo">
+        </div>
+        <div class="nav">
+            <ul>
+                <a href="?page=Home">
+                    <li>Home</li>
+                </a>
+                <a href="?page=About">
+                    <li>About</li>
+                </a>
+                <a class="hide" href="?page=EngineeredHardwood">
+                    <li class="hide">Engineered Hardwood</li>
+                </a>
 
-                    <a class="hide" href="?page=Laminate">
-                        <li class="hide">Laminate</li>
-                    </a>
-                    <a class="hide" href="?page=VinylPlank">
-                        <li class="hide">Vinyl Plank</li>
-                    </a>
+                <a class="hide" href="?page=Laminate">
+                    <li class="hide">Laminate</li>
+                </a>
+                <a class="hide" href="?page=VinylPlank">
+                    <li class="hide">Vinyl Plank</li>
+                </a>
 
-                    <a class="show-mq" href="">
-                        <li class="show-mq">Flooring Types</li>
-                    </a>
-                    <a href="?page=Contact">
-                        <li>Contact</li>
-                    </a>
+                <a class="show-mq" href="">
+                    <li class="show-mq">Flooring Types</li>
+                </a>
+                
+                <ul class="dropdown">
+                    <li class="drop-item"><a href="">**********</a></li>
+                    <li class="drop-item"><a href="">**********</a></li>
+                    <li class="drop-item"><a href="">**********</a></li>
                 </ul>
-                <div class="wrapper2">
-                    <p class="lic-info">Matthew Raymond</p>
-                    <p class="lic-info2">License #768883</p>
-                </div>
+             
+                <a href="?page=Contact">
+                    <li>Contact</li>
+                </a>
+            </ul>
+            <div class="wrapper2">
+                <p class="lic-info">Matthew Raymond</p>
+                <p class="lic-info2">License #768883</p>
             </div>
-        </nav>
+        </div>
+    </nav>
 </header>
