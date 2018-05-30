0d4f8c9482fb89b070aa8154729face31d6ce973
Roman Andy <150039017@cart.ads>
2018-05-30 08:50:33 -0700

    'r'

--- pages/EngineeredHardwood.php
+++ pages/EngineeredHardwood.php
@@ -8,7 +8,7 @@ while($row = mysqli_fetch_assoc($result)) {
 }
 ?>
 <main class="content">
-	<div style="background-image: linear-gradient(rgba(0, 0, 0, .2), rgba(0, 0, 0, .2)), url(images/hardwood4.png); width: 100%; height: 570px;">
+	<div style="background-image: linear-gradient(rgba(0, 0, 0, .2), rgba(0, 0, 0, .2)), url(images/hardwood4.jpg); width: 100%; height: 570px;">
         <h2 class="headerText">Engineered Hardwood</h2>
 	</div>
 	<div style="width:40%; margin: 0 auto;">
